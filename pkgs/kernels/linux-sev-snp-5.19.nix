efaea912f01ce402d3639d1da8b212ed940dbb77
Masanori Misono <m.misono760@gmail.com>
2023-02-14 12:23:44 +0000

    sev-snp: use custom kernel
    This kernel contains fixes of perf build error due to binutils updates

--- pkgs/kernels/linux-sev-snp-5.19.nix
+++ pkgs/kernels/linux-sev-snp-5.19.nix
@@ -1,4 +1,4 @@
-{ buildLinux, fetchFromGitHub, modDirVersionArg ? null, ... }@args:
+{ buildLinux, fetchFromGitHub, modDirVersionArg ? "5.19.0-rc5-next-20220706", ... }@args:
 
 buildLinux (args // rec {
   version = "5.19";
@@ -8,11 +8,17 @@ buildLinux (args // rec {
     else
       modDirVersionArg;
   src = fetchFromGitHub {
-    owner = "AMDESE";
-    repo = "linux";
     # https://github.com/AMDESE/linux/tree/sev-snp-5.19-rc5-v2
-    rev = "8e4a0b83a7b0a312efc8a091c0d6d2d920049e5b";
-    sha256 = "sha256-A6UYI+Xo0uJh+KfUcVR/2Bi+m269rikoDs0Snvnf0Rg=";
+    # owner = "AMDESE";
+    # repo = "linux";
+    # rev = "8e4a0b83a7b0a312efc8a091c0d6d2d920049e5b";
+    # sha256 = "sha256-A6UYI+Xo0uJh+KfUcVR/2Bi+m269rikoDs0Snvnf0Rg=";
+
+    # https://github.com/mmisono/linux/tree/sev-snp-5.19-rc5-v2-dev
+    owner = "mmisono";
+    repo = "linux";
+    rev = "671ad6d15cf883ae29e8c9613aa4dbbdd71244d7";
+    sha256 = "sha256-XzTXafyv/tIIhBLPp2KsBTYd5otVlWr1fgy736mAZLw=";
   };
 
   kernelPatches = [
