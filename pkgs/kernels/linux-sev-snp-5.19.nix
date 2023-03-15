4d1afdf8c18f128998649132e50f6abdcafb176b
Masanori Misono <m.misono760@gmail.com>
2023-03-15 10:53:46 +0000

    sev-snp: add latest development kernel for normal snp

--- pkgs/kernels/linux-sev-snp-5.19.nix
+++ pkgs/kernels/linux-sev-snp-5.19.nix
@@ -1,35 +1,38 @@
 { buildLinux, fetchFromGitHub, ... }@args:
 let
-  # the original rc5 version (not work)
+  # the original 5.19-rc5 version (not work)
   # This failes to build due to binutils update
   # https://github.com/AMDESE/linux/tree/sev-snp-5.19-rc5-v2
-  # rc5 = {
+  # snp_5_19_rc5 = {
   #   owner = "AMDESE";
   #   repo = "linux";
   #   rev = "8e4a0b83a7b0a312efc8a091c0d6d2d920049e5b";
   #   sha256 = "sha256-A6UYI+Xo0uJh+KfUcVR/2Bi+m269rikoDs0Snvnf0Rg=";
+  #   version = "5.19";
   #   modDirVersionArg = "5.19.0-rc5-next-20220706";
   #   extraPatches = [];
   # };
 
-  # rc5 version, wich bunitls patches
+  # 5.19-rc5 version, wich bunitls patches
   # https://github.com/mmisono/linux/tree/sev-snp-5.19-rc5-v2-dev
-  rc5 = {
+  snp_5_19_rc5 = {
     owner = "mmisono";
     repo = "linux";
     rev = "671ad6d15cf883ae29e8c9613aa4dbbdd71244d7";
     sha256 = "sha256-XzTXafyv/tIIhBLPp2KsBTYd5otVlWr1fgy736mAZLw=";
+    version = "5.19";
     modDirVersionArg = "5.19.0-rc5-next-20220706";
     extraPatches = [];
   };
 
-  # rc6 version, with binutils patches
+  # 5.19-rc6 version, with binutils patches
   # https://github.com/mmisono/linux/tree/sev-snp-iommu-avic_5.19-rc6_v4-dev
-  rc6 = {
+  snp_5_19_rc6 = {
     owner = "mmisono";
     repo = "linux";
     rev = "92221f6d4d09653d0a6787d0906958e6d884b85c";
     sha256 = "sha256-2gHPMA84dWBnxMvL4Bmky0c5onDizcr48sPpZpDU79g=";
+    version = "5.19";
     modDirVersionArg = "5.19.0-rc6";
     extraPatches = [
       {
@@ -43,13 +46,38 @@ let
     ];
   };
 
+  # RFC v8 https://lwn.net/Articles/923844/
+  # https://github.com/AMDESE/linux/tree/upmv10-host-snp-v8-rfc
+  snp_6_1_rfc_v8 = {
+    owner = "AMDESE";
+    repo = "linux";
+    rev = "90eb54a5140b430f4c20c25deadd59f04485d2b2";
+    sha256 = "sha256-M/20L2gS8Mg3iLRq00PrlwXyxGD+IEzgcm8/GXWvIBw=";
+    version = "6.1";
+    modDirVersionArg = "6.1.0-rc4";
+    extraPatches = [];
+  };
+
+  # snp host latest branch
+  # https://github.com/AMDESE/linux/tree/snp-host-latest
+  snp_latest = {
+    owner = "AMDESE";
+    repo = "linux";
+    rev = "db73108c4fd62c03ad57e0c7118e5623750898ee";
+    sha256 = "sha256-HP2U6xtwqZQgzJd/RH2I3Ph5wKVkhXtVajNALf4R4HQ=";
+    version = "6.1";
+    modDirVersionArg = "6.1.0-rc4";
+    extraPatches = [];
+  };
+
   # change here to change kernel
-  snp_kernel = rc5;
-  # snp_kernel = rc6;
+  # snp_kernel = snp_5_19_rc6;
+  # snp_kernel = snp_6_1_rfc_v8;
+  snp_kernel = snp_latest;
 
 in with snp_kernel;
 buildLinux (args // rec {
-  version = "5.19";
+  inherit version;
   modDirVersion =
     if (snp_kernel.modDirVersionArg == null) then
       builtins.replaceStrings [ "-" ] [ ".0-" ] version
@@ -71,9 +99,10 @@ buildLinux (args // rec {
         CRYPTO_DEV_SP_PSP y
         KVM_AMD_SEV y
         MEMORY_FAILURE y
+        EXPERT y
       '';
     }
   ] ++ extraPatches;
-  extraMeta.branch = "5.19";
+  extraMeta.branch = version;
   ignoreConfigErrors = true;
 } // (args.argsOverride or { }))
